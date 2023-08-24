a57cd249a38b6e363d6e77638e140399c8722732
Samuele Hajar <samuele.hajar@gmail.com>
2023-08-24 17:16:35 -0400

    Shortened code

--- Arrays & Hashing/ContainsDuplicate.js
+++ Arrays & Hashing/ContainsDuplicate.js
@@ -1,6 +1,4 @@
 var containsDuplicate = (nums) => {
     const numsSet = new Set(nums); //a set can contain only unique values
-    const equalCheck = numsSet.size === nums.length;
-
-    return !equalCheck;
+   return !(numsSet.size === nums.length); //return the opposite
 };
